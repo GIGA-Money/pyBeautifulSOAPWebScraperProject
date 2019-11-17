5d0dd3d3293b6c25b308aa79248c818ed9d256a2
GIGA-Money <christopherregan@outlook.com>
2019-11-17 18:53:04 -0500

    updated to use py odbc, but still issues

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
 import ssl
+from urllib.request import Request, urlopen
+
+from bs4 import BeautifulSoup
+import pyodbc
+import json
 
 site= "https://pastebin.com/raw/Bjsw46PD"
 hdr = {'User-Agent': 'Mozilla/5.0'}
@@ -8,7 +11,8 @@ context = ssl._create_unverified_context()
 req = Request(site, headers=hdr)
 page = urlopen(req, context=context)
 soup = BeautifulSoup(page,"html.parser")
-#print(soup)
+
+medstr = {}
 medstr = str(soup)
 medstr = medstr.replace('has 1 vehicle Directives.\r\n', '')
 medstr = medstr.replace('has 2 vehicle Directives.\r\n', '')
@@ -21,22 +25,32 @@ medstr = medstr.replace('has 8 vehicle Directives.\r\n', '')
 medstr = medstr.replace('#', '')
 medstr = medstr.split(' ')
 
-evens = 0
-odds = 1
-indexlist = []
-valuelist = []
+values = []
+entity = []
 
-for element in  medstr:
-    print(element)
-    if medstr[evens] == 0:
-        print(element)
+index = 0
+for element in medstr:
+    if (index % 2) == 0:
+        values.append(element)
+    else:
+        entity.append(element)
+    index += 1
 
-#pageNum = 1
-#+ str(pageNum)
-#html1 = urlopen("https://ps4us.ps2.fisu.pw/leaderboard/player/#acc")
-#html2 = urlopen("https://ps2.fisu.pw/leaderboard/player/#acc")
-#print(html2.read())
-#while pageNum:
- #   pageNum += 1
-  #  if pageNum > 100:
-   #     pageNum = False
+listToDict = {'index': values, 'names': entity}
+print(json.dumps(listToDict))
+
+
+
+#connString = 'Trusted_Connection=no;DRIVER={ODBC Driver 18 for SQL Server};' \
+#             'SERVER=@localhost\\MySQL;PORT=3306;DATABASE=test;UID=root;PWD=;'
+#try:
+#    cn = pyodbc.connect(connString)
+ #   print('You are connected to db ')
+
+#except Exception as e:
+ #   print('Error connecting to databse: ', str(e))
+
+#finally:
+ #   cn.close()
+  #  print('Connection closed')
+
