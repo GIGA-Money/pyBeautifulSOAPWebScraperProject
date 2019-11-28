a5ca5d2753d26ea54ba91dcbee35d637935e9e7f
Christopher Michael Regan <gigachris1@hotmail.com>
2019-11-28 11:34:12 -0500

    updated to working condition with main.py

--- main.py
+++ main.py
@@ -3,58 +3,70 @@ from urllib.request import Request, urlopen
 
 from bs4 import BeautifulSoup
 import mysql.connector
-import json
 
-localhostdb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="rooter",
-    database="player_entry"
-)
 
-activecursor = localhostdb.cursor()
+def sprint_string():
+    index = 0
+    for element in formattedString:
+        if (index % 2) == 0:
+            values.append(element)
+            print(element)
+        else:
+            entity.append(element)
+            print(element)
+        index += 1
 
-activecursor.execute("create table players(ID varcharacter(15), player_name varcharacter(255), primary key (ID))")
-#activecursor.execute("insert into player_entry.players values ('1','player')")
 
-site= "https://pastebin.com/raw/Bjsw46PD"
+def text_format():
+    fString = str(soup)
+    fString = fString.replace('has 1 vehicle Directives.\r\n', '')
+    fString = fString.replace('has 2 vehicle Directives.\r\n', '')
+    fString = fString.replace('has 3 vehicle Directives.\r\n', '')
+    fString = fString.replace('has 4 vehicle Directives.\r\n', '')
+    fString = fString.replace('has 5 vehicle Directives.\r\n', '')
+    fString = fString.replace('has 6 vehicle Directives.\r\n', '')
+    fString = fString.replace('has 7 vehicle Directives.\r\n', '')
+    fString = fString.replace('has 8 vehicle Directives.\r\n', '')
+    fString = fString.replace('#', '')
+    fString = fString.split(' ')
+    return fString
+
+site = "https://pastebin.com/raw/Bjsw46PD"
 hdr = {'User-Agent': 'Mozilla/5.0'}
 context = ssl._create_unverified_context()
 req = Request(site, headers=hdr)
 page = urlopen(req, context=context)
-soup = BeautifulSoup(page,"html.parser")
-
-medstr = {}
-medstr = str(soup)
-medstr = medstr.replace('has 1 vehicle Directives.\r\n', '')
-medstr = medstr.replace('has 2 vehicle Directives.\r\n', '')
-medstr = medstr.replace('has 3 vehicle Directives.\r\n', '')
-medstr = medstr.replace('has 4 vehicle Directives.\r\n', '')
-medstr = medstr.replace('has 5 vehicle Directives.\r\n', '')
-medstr = medstr.replace('has 6 vehicle Directives.\r\n', '')
-medstr = medstr.replace('has 7 vehicle Directives.\r\n', '')
-medstr = medstr.replace('has 8 vehicle Directives.\r\n', '')
-medstr = medstr.replace('#', '')
-medstr = medstr.split(' ')
+soup = BeautifulSoup(page, "html.parser")
+formattedString = text_format()
 
 values = []
 entity = []
 
-index = 0
-for element in medstr:
-    if (index % 2) == 0:
-        values.append(element)
-    else:
-        entity.append(element)
-    index += 1
+sprint_string()
 
-listToDict = {'index': values, 'names': entity}
-print(json.dumps(listToDict))
+localhost = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="rooter",
+        database="player_entry"
+    )
+activeCursor = localhost.cursor()
 
+try:
 
-#
-#attmpts at using odbc over mysql connector
-#This will print the drivers installed with the py odbc install.
-#for driver in pyodbc.drivers():
-#    print(driver)
-#
+    activeCursor.execute("create table if not exists players(ID varcharacter(15), "
+                         "player_name varcharacter(255), primary key (ID))")
+    #
+    # values (index, player name)
+    #
+
+    for vdex in values:
+        for edex in entity:
+            activeCursor.execute("insert into player_entry.players values ('%s','%s')" % (vdex, edex))
+
+except mysql.connector.Error as errors:
+    print(errors)
+finally:
+    activeCursor.commit()
+    if activeCursor: activeCursor.close()
+    if localhost: localhost.close()
