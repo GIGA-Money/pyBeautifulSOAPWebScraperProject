98792788e4bf038de01c7168df65fbb0737f9e68
Christopher Michael Regan <gigachris1@hotmail.com>
2019-11-28 15:40:11 -0500

    updated main to organize methods and format comments

--- main.py
+++ main.py
@@ -5,31 +5,56 @@ from bs4 import BeautifulSoup
 import mysql.connector
 
 
-def sprint_string():
+#
+# This will format the string, by removing excess text that isn't used.
+#
+
+
+def text_format():
+    f_string = str(soup)
+    f_string = f_string.replace('has 1 vehicle Directives.\r\n', '')
+    f_string = f_string.replace('has 2 vehicle Directives.\r\n', '')
+    f_string = f_string.replace('has 3 vehicle Directives.\r\n', '')
+    f_string = f_string.replace('has 4 vehicle Directives.\r\n', '')
+    f_string = f_string.replace('has 5 vehicle Directives.\r\n', '')
+    f_string = f_string.replace('has 6 vehicle Directives.\r\n', '')
+    f_string = f_string.replace('has 7 vehicle Directives.\r\n', '')
+    f_string = f_string.replace('has 8 vehicle Directives.\r\n', '')
+    f_string = f_string.replace('#', '')
+    f_string = f_string.split(' ')
+    return f_string
+
+
+#
+# This method extracts the index from the name into 2 separate list.
+#
+
+def splint_string():
     index = 0
     for element in formattedString:
         if (index % 2) == 0:
             values.append(element)
-            print(element)
+        # print(element)
         else:
             entity.append(element)
-            print(element)
+        # print(element)
         index += 1
 
 
-def text_format():
-    fString = str(soup)
-    fString = fString.replace('has 1 vehicle Directives.\r\n', '')
-    fString = fString.replace('has 2 vehicle Directives.\r\n', '')
-    fString = fString.replace('has 3 vehicle Directives.\r\n', '')
-    fString = fString.replace('has 4 vehicle Directives.\r\n', '')
-    fString = fString.replace('has 5 vehicle Directives.\r\n', '')
-    fString = fString.replace('has 6 vehicle Directives.\r\n', '')
-    fString = fString.replace('has 7 vehicle Directives.\r\n', '')
-    fString = fString.replace('has 8 vehicle Directives.\r\n', '')
-    fString = fString.replace('#', '')
-    fString = fString.split(' ')
-    return fString
+#
+# This will insert the values into the database. Using the existing lists, values and entity.
+#
+
+
+def insertion():
+    for vdex in values:
+        for edex in entity:
+            activeCursor.execute("insert into players values ('%s','%s')" % (vdex, edex))
+
+#
+# All of the global variables used below.
+#
+
 
 site = "https://pastebin.com/raw/Bjsw46PD"
 hdr = {'User-Agent': 'Mozilla/5.0'}
@@ -42,14 +67,17 @@ formattedString = text_format()
 values = []
 entity = []
 
-sprint_string()
+splint_string()
+
+# Connect to the mysql db.
+
 
 localhost = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="rooter",
-        database="player_entry"
-    )
+    host="localhost",
+    user="root",
+    passwd="rooter",
+    database="player_entry"
+)
 activeCursor = localhost.cursor()
 
 try:
@@ -60,9 +88,7 @@ try:
     # values (index, player name)
     #
 
-    for vdex in values:
-        for edex in entity:
-            activeCursor.execute("insert into player_entry.players values ('%s','%s')" % (vdex, edex))
+    insertion()
 
 except mysql.connector.Error as errors:
     print(errors)
